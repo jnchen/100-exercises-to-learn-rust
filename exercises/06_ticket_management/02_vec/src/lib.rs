d10e69c267ea725a0a05a58f79a933b52c35c4dd
caojingchen <42629624@qq.com>
2024-08-09 15:17:30 +0800

    finish 06-02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let idx = n as usize;
+    let mut memory = vec![0, 1];
+    for i in 2..=idx{
+        memory.push(memory[i -1] + memory[i -2]);
+    }
+    memory[idx]
 }
 
 #[cfg(test)]
