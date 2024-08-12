af98ea382f40ca0617adea77e36eaa29a7e947f6
caojingchen <42629624@qq.com>
2024-08-12 16:17:54 +0800

    finish 07-10

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -35,7 +35,11 @@ impl TicketStoreClient {
         Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {}
+    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {
+        let (response_sender, reponse_receiver) =  sync_channel(1);
+        self.sender.try_send(Command::Update { patch: ticket_patch, response_channel: response_sender }).map_err(|_| OverloadedError)?;
+        Ok(reponse_receiver.recv().unwrap())
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -85,7 +89,20 @@ pub fn server(receiver: Receiver<Command>) {
                 patch,
                 response_channel,
             }) => {
-                todo!()
+                let ticket = store.get_mut(patch.id).unwrap();
+                match patch.title{
+                    Some(data) => ticket.title = data.into(),
+                    _ => {}
+                };
+                match patch.description{
+                    Some(data) => ticket.description = data.into(),
+                    _ => {}
+                };
+                match patch.status {
+                    Some(data) => ticket.status = data,
+                    _ => {}
+                }
+                let _ = response_channel.send(());
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
